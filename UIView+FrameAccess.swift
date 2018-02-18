47f6e5762dc1ec6e06fefbe404cfa3ec3f2e5c7a
野瀬田裕樹 <yuukiw00w@gmail.com>
2018-02-18 21:53:37 +0900

    [modify]SwiftLint対応（空行のスペース削除、variable_nameの警告抑制）

--- UIView+FrameAccess.swift
+++ UIView+FrameAccess.swift
@@ -13,47 +13,47 @@ extension UIView {
         get { return frame.origin }
         set { frame.origin = newValue }
     }
-    
+
     var viewSize: CGSize {
         get { return frame.size }
         set { frame.size = newValue }
     }
-    
-    var x: CGFloat {
+
+    var x: CGFloat { // swiftlint:disable:this variable_name
         get { return frame.origin.x }
         set { frame.origin = CGPoint(x: newValue, y: frame.origin.y) }
     }
-    
+
     var right: CGFloat {
         get { return frame.origin.x + frame.size.width }
         set { frame.origin = CGPoint(x: newValue - frame.size.width, y: frame.origin.y) }
     }
-    
-    var y: CGFloat {
+
+    var y: CGFloat { // swiftlint:disable:this variable_name
         get { return frame.origin.y }
         set { frame.origin = CGPoint(x: frame.origin.x, y: newValue) }
     }
-    
+
     var bottom: CGFloat {
         get { return frame.origin.y + frame.size.height }
         set { frame.origin = CGPoint(x: frame.origin.x, y: newValue - frame.size.height) }
     }
-    
+
     var width: CGFloat {
         get { return frame.size.width }
         set { frame.size = CGSize(width: newValue, height: frame.size.width) }
     }
-    
+
     var height: CGFloat {
         get { return frame.size.height }
         set { frame.size = CGSize(width: frame.size.height, height: newValue) }
     }
-    
+
     var centerX: CGFloat {
         get { return center.x }
         set { center = CGPoint(x: newValue, y: center.y) }
     }
-    
+
     var centerY: CGFloat {
         get { return center.y }
         set { center = CGPoint(x: center.x, y: newValue) }
